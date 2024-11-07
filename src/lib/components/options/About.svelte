6f138201f04b6d8324dba5080bb7a2bff9599659
Albin Carlsson <github.garter593@passmail.net>
2024-11-07 12:07:34 +0100

    rate us btn + build.zip

--- src/lib/components/options/About.svelte
+++ src/lib/components/options/About.svelte
@@ -2,14 +2,17 @@
 	import * as Card from '$lib/components/ui/card/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import GithubLogo from 'svelte-radix/GithubLogo.svelte';
+	import Star from 'svelte-radix/Star.svelte';
 	import FormField from '$lib/components/options/FormField.svelte';
 	import { getExtensionVersion } from '$lib/chrome/utils';
 	import Kofi from '$lib/images/kofi.webp';
+	import { Separator } from '$lib/components/ui/separator/index.js';
 </script>
 
 <Card.Root>
 	<Card.Content class="mt-4 space-y-2">
 		<div class="flex flex-col gap-4">
+			<legend class="text-base font-medium">Extension</legend>
 			<FormField desc="Version">
 				{#await getExtensionVersion() then version}
 					<span class="text-xs">{version}</span>
@@ -20,6 +23,9 @@
 					<GithubLogo size="16" />
 				</Button>
 			</FormField>
+
+			<Separator orientation="horizontal" class="my-2" />
+			<legend class="text-base font-medium">Support</legend>
 			<FormField desc="Support me on Ko-fi">
 				<Button
 					href="https://ko-fi.com/albincarlsson"
@@ -28,6 +34,16 @@
 					size="icon"
 				>
 					<img src={Kofi} alt="Support me on Ko-fi" height="16" class="h-4" />
+				</Button>
+			</FormField>
+			<FormField desc="Rate the extension on Chrome Web Store">
+				<Button
+					href="https://chromewebstore.google.com/detail/simple-tab-manager/mdfbfcbfcohpbdicnpdpcdioggfdddlc/reviews"
+					target="_blank"
+					variant="outline"
+					size="icon"
+				>
+					<Star size="16" />
 				</Button>
 			</FormField>
 		</div>
